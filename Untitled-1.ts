89587b6782d7db0183f701f78af6f2e3e8aa5785
Burhan1431 <164791035+Burhan1431@users.noreply.github.com>
2024-05-14 15:24:28 +0500

    Add files via upload

---
+++ Untitled-1.ts
@@ -0,0 +1,48 @@
+// ----------------question no 1---------------//
+
+function logHelloWorld() {
+    console.log("Hello World");}
+
+    logHelloWorld();
+// ----------------question no 2--------------//
+
+
+let temperature = 18; 
+
+if (temperature < 20) {
+    console.log("It's cold!");
+}
+
+
+//------------------- question no 3-------------------//
+
+
+let TotalApples = 10;
+
+let ApplesGiven= 3;
+
+let applesLeft = TotalApples - ApplesGiven;
+
+console.log("Remaining Apples are ",applesLeft);
+
+// -----------------question no 4----------------------//
+
+
+let firstName = "Burhan";
+let lastName = "Qadeer";
+
+let fullName = firstName + lastName;
+
+console.log("Full Name is :-", fullName);
+
+
+
+//------------------- question no 5---------------------//
+let a = 5;
+let b = 3;
+
+if (a > b) {
+    console.log("Yes");
+} else {
+    console.log("No");
+}
